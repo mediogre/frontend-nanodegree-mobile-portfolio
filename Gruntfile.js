60602563d8152e86b3dee5ad156f708abfbb0df0
Ed Sinjiashvili <ed.sinjiashvili@gmail.com>
2015-02-04 00:43:05 +0300

    Improved copying and added cleanup: 	- grunt copy only copies inside of src/ without creating unnecessary directory 	- grunt clean removes build dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     copy: {
       main: { 
-        src: 'src/*',
+	expand: true,
+	cwd: 'src/',
+        src: ['**'],
         dest: 'build/'
       }
-    }
+    },
+    clean: ['build/']
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', function() {
     grunt.log.writeln("Hi there");
